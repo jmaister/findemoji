5252954504797c3ef4da8f7343c86a8a39507c3f
Jordi Burgos <jordiburgos@gmail.com>
2017-12-13 21:34:42 +0000

    Fix format.

--- client/index.jsx
+++ client/index.jsx
@@ -4,12 +4,12 @@ import { render } from 'react-dom';
 import ButtonMatrix from './ButtonMatrix.jsx';
 
 class App extends React.Component {
-  render() {
-    return [
-      "Hello React!",
-      <ButtonMatrix key="matrix" side="5" />
-    ];
-  }
+    render() {
+        return [
+            "Hello React!",
+            <ButtonMatrix key="matrix" side="5" />
+        ];
+    }
 }
 
 render(<App />, document.getElementById('app'));
